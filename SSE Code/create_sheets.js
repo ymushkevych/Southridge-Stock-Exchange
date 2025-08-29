f1d6e0fa86ffe5c06f798010c612b4ddb3319ba4
Yarema Mushkevych <ymushkevych@gmail.com>
2025-08-29 11:56:00 -0700

    Update create_sheets.js

--- SSE Code/create_sheets.js
+++ SSE Code/create_sheets.js
@@ -1,79 +1,47 @@
-let names = [];
-let emails = [];
-let passwords = [];
+function createSheets() {
+  var ids = [];
+  var emails = [];
+  var passwords = [];
+  var names = [];
 
-function setup() {
+  const responseSheet = SpreadsheetApp.openById().getSheetByName('Form Responses 1'); //link the google form used to collect responses into a sheet and copy-paste the ID
 
-  var signupForm = FormApp.openById();
-  
-  ScriptApp.newTrigger("configureNamesAndEmails")
-  .forForm(signupForm)
-  .onFormResponse()
-  .create();
-}
+  const signupForm = FormApp.openById(); //signup form ID
 
-function configureNamesAndEmails() {
-
-  var signupForm = FormApp.openById();
-
-  const responses = form.getResponses();
+  const responses = signupForm.getResponses();
   const formLength = responses.length;
-  const target = responses[formLength - 1];
-  const items = form.getItems();
-
-  const nameQ = "What is your name (First and Last)";
-  const emailQ = "What is your email?";
-  const passwordQ = "What do you want your password to be?";
-
-  var nameIndex = items.findIndex(nameList => nameList.getTitle() === nameQ);
-  var nameList = items[nameIndex];
-  var name = target.getResponseForItem(nameList).getResponse();
-
-  var emailIndex = items.findIndex(emailList => emailList.getTitle() === emailQ);
-  var emailList = items[emailIndex];
-  var email = target.getResponseForItem(emailList).getResponse();
-
-  var passwordIndex = items.findIndex(passwordList => passwordList.getTitle() === passwordQ);
-  var passwordList = items[passwordIndex];
-  var password = target.getResponseForItem(passwordList).getResponse();
 
 
-  names.push(name);
-  emails.push(email);
-  passwords.push(password);
-
-  createSheets(names, emails, passwords);  
-}
-
-function createSheets(names, emails, passwords) {
-  var ids = []; //do not edit
-
-  for (var i = 0; i < emails.length; i++) {
-    var templateSheet = DriveApp.getFileById();
-    var templateCopy = templateSheet.makeCopy("Individual Portfolio Sheet: " + names[i]);
+  for (var i = 0; i < formLength; i++) {
+    var templateSheet = DriveApp.getFileById(); // design an example portfolio
+    var templateCopy = templateSheet.makeCopy("Individual Portfolio Sheet: " + responseSheet.getRange(i+2, 2).getValue());
     var portfolioSheet = SpreadsheetApp.open(templateCopy);
     var tab = portfolioSheet.getActiveSheet();
     tab.setName('portfolio');
-    tab.getRange("A1").setValue(names[i]);
-    tab.getRange("I2").setValue(passwords[i])
-    
+    tab.getRange("A1").setValue(responseSheet.getRange(i+2, 2).getValue());
+    tab.getRange("I2").setValue(responseSheet.getRange(i+2, 4).getValue());
+
     var driveFile = DriveApp.getFileById(portfolioSheet.getId());
-    driveFile.addEditor(emails[i]);
+    driveFile.addEditor(responseSheet.getRange(i+2, 3).getValue());
 
     var protection = tab.protect().setDescription("protected range");
-    protection.addEditor(emails[i]);
+    protection.addEditor(responseSheet.getRange(i+2, 3).getValue());
     var unprotected = tab.getRange('G5:G7');
     protection.setUnprotectedRanges([unprotected]);
 
     var id = portfolioSheet.getId();
-
     ids.push(id);
+
+    // add password to main exchange sheet
+
+    const exchange = SpreadsheetApp.openById();
+
+    exchange.getSheetByName('Passwords').getRange(i+10, 2).setValue(responseSheet.getRange(i+2, 4).getValue());
+    exchange.getSheetByName('Passwords').getRange(i+10, 1).setValue(responseSheet.getRange(i+2, 2).getValue());
   }
 
-  console.log(ids);
-  console.log(passwords);
-  console.log(emails);
   console.log(names);
-
-  // copy-paste the lists that appear (you can view them in Executions → configureNamesAndEmails. If you already have other scripts running, you can use ctrl+f or find-in-page to help
+  console.log(emails);
+  console.log(passwords);
+  
 }
