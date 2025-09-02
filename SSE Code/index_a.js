2af96f25a32f5db2af4191540456bd5f29fc19f6
Yarema Mushkevych <ymushkevych@gmail.com>
2025-09-02 14:32:03 -0700

    Update index_a.js

--- SSE Code/index_a.js
+++ SSE Code/index_a.js
@@ -54,7 +54,7 @@ function setIndexA() {
 
   // calculate group bias as the proportion of total stock trades that a stock makes up, and output that value in column D.
   // replace 41 with the final row of the Index A sheet
-  var biasIncrease = index.getSheetByName('Index A').getRange('B2:B41').getValues().flat().filter(String);  
+  var biasIncrease = index.getSheetByName('Index A').getRange(2, 2, stockCount, 1).getValues().flat().filter(String);  
   var dataList = exchange.getSheetByName('SSE Form Responses').getRange('C2:C').getValues().flat();
   for (var i = 0; i < stockCount; i++) {
     var currentItem = biasIncrease[i];
